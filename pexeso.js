224a15ce65879717f978b800c961dc7f22c0151a
maruška <93185278+maruska7@users.noreply.github.com>
2023-12-05 13:06:01 +0100

    pexeso zacatek

--- pexeso.js
+++ pexeso.js
@@ -18,11 +18,18 @@ setTimeout(myfnc, 3000);
 
 let card1 = document.getElementById('card-1');
 let card2 = document.getElementById('card-2');
+let card3 = document.getElementById('card-3');
+let card4 = document.getElementById('card-4');
+let card5 = document.getElementById('card-5');
+let card6 = document.getElementById('card-6');
+let card7 = document.getElementById('card-7');
+let card8 = document.getElementById('card-8');
 
 function clickCard1() {
-    card1.innerHTML = '<img src="img/banaan-fruit-geisoleerde.png" alt="banan">';
+    card1.innerHTML = '<img src="img/banan.png" alt="banan">';
 }
 
 function clickCard2() {
-    card2.innerHTML = '<img src="img/banaan-fruit-geisoleerde.png" alt="banan">';
+    card2.innerHTML = '<img src="img/banan.png" alt="banan">';
 }
+
